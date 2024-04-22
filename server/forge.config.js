95dc45be6e99ca5c8cfa9a9ca0c340cbf3df64fe
mxxnsk <mxxnsk@gmail.com>
2024-04-21 17:25:19 -0700

    Add binaries signing config

--- server/forge.config.js
+++ server/forge.config.js
@@ -3,13 +3,28 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 
 module.exports = {
   packagerConfig: {
+    osxSign: {},
+    // ...
+    osxNotarize: {
+      tool: 'notarytool',
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID
+    }
+    // ...
+  },
+
+  packagerConfig: {
     asar: true,
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        certificateFile: './cert.pfx',
+        certificatePassword: process.env.CERTIFICATE_PASSWORD
+      }
     },
     {
       name: '@electron-forge/maker-zip',
