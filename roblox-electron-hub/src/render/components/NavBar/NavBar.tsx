0c3e2bbf3dd40a288d23defc685f20c7ab6ec051
Evgeny Sinitsyn <cuhuak@gmail.com>
2024-10-07 14:42:19 +0700

    (chore) make navbar narrow

--- roblox-electron-hub/src/render/components/NavBar/NavBar.tsx
+++ roblox-electron-hub/src/render/components/NavBar/NavBar.tsx
@@ -66,7 +66,7 @@ export default function Simple() {
   return (
     <>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+        <Flex h={12} alignItems={'center'} justifyContent={'space-between'}>
           <IconButton
             size={'md'}
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
