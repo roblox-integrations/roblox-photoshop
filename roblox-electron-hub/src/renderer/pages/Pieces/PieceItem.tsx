49b4d89a49ae29a0220f04bc8adb5c50a441218f
Evgeny Sinitsyn <cuhuak@gmail.com>
2024-11-11 00:59:28 +0700

    (chore) design piece item

--- roblox-electron-hub/src/renderer/pages/Pieces/PieceItem.tsx
+++ roblox-electron-hub/src/renderer/pages/Pieces/PieceItem.tsx
@@ -1,7 +1,9 @@
-import {Switch} from '@/components/ui/switch'
 import {Field} from '@/components/ui/field'
-import {Button, Card, Code, Flex, Heading, Image, Stack, Text} from '@chakra-ui/react'
+import {Switch} from '@/components/ui/switch'
+import {Box, Card, Code, Flex, Heading, IconButton, Image, VStack} from '@chakra-ui/react'
+
 import {useState} from 'react'
+import {MdOutlineDelete as MdDelete, MdOutlineFolder as MdFolder, MdOutlineCloudUpload as MdUpload} from 'react-icons/md'
 
 function PieceItemCurrentAssetId({item}) {
   const found = item?.uploads?.find(x => x.fileHash === item.fileHash)
@@ -11,16 +13,15 @@ function PieceItemCurrentAssetId({item}) {
   }
 
   return (
-    <>
-      assetId=
-      {found.assetId}
-    </>
+    <Code colorPalette="blue">
+      rbxassetid://{found.assetId}
+    </Code>
   )
 }
 
 function PieceItemDate({date}) {
   const result = (new Date(date * 1000)).toISOString().slice(0, 19)
-  return <>{result}</>
+  return <Code colorPalette="gray">{result}</Code>
 }
 
 export default function PieceItem({item}) {
@@ -57,7 +58,7 @@ export default function PieceItem({item}) {
     console.log('[PieceItem] deleted', json)
   }
 
-  const onCreateAsset = async () => {
+  const onUpload = async () => {
     const res = await fetch(`http://localhost:3000/api/pieces/${item.id}/asset`, {
       method: 'POST',
       headers: {
@@ -84,6 +85,7 @@ export default function PieceItem({item}) {
       overflow="hidden"
       size="sm"
       variant="outline"
+      gap="1"
     >
       <Image
         objectFit="cover"
@@ -92,52 +94,70 @@ export default function PieceItem({item}) {
         alt="Caffe Latte"
       />
 
-      <Stack>
-        <Card.Body>
+      <Box flex="1 0 auto">
+        <Card.Body p="2">
           <Flex gap="2" my="2">
-            <Code colorPalette="blue">{item.role}</Code>
-            <Code colorPalette="green">{item.type}</Code>
-            <Code colorPalette="gray">
-              #{item.id}
+            <Code colorPalette="gray" variant="surface">
+              #
+              {item.id}
             </Code>
-            <Code colorPalette="gray"><PieceItemDate date={item.updatedAt}></PieceItemDate></Code>
+            <Code colorPalette="green">{item.type}</Code>
+            <PieceItemDate date={item.updatedAt}></PieceItemDate>
+            <PieceItemCurrentAssetId item={item}></PieceItemCurrentAssetId>
+
           </Flex>
           <Heading size="xs">{item.filePath}</Heading>
-          <Text>
-            hash:
-            {' '}
-            {item.fileHash}
-          </Text>
-          <Text>
-            uploads=
-            {item.uploads.length}
-            {' '}
-            <PieceItemCurrentAssetId item={item}></PieceItemCurrentAssetId>
-          </Text>
-
+          {/*
+          <Text>hash: {item.fileHash}</Text>
+          <Text>uploads= {item.uploads.length} <PieceItemCurrentAssetId item={item}></PieceItemCurrentAssetId></Text>
+*/}
         </Card.Body>
-
         <Card.Footer gap="2" alignItems="center">
-          <Field>
-            <Switch name="qwe" checked={isAutoSave} onChange={onChangeIsAutoSave}>
-              auto save
-            </Switch>
-          </Field>
-
-          <Button variant="outline" colorPalette="blue" size="sm" onClick={onReveal}>
-            Show in explorer (finder)
-          </Button>
-
-          <Button variant="outline" colorPalette="blue" size="sm" onClick={onCreateAsset}>
-            Create Asset
-          </Button>
-
-          <Button variant="outline" colorPalette="red" size="sm" onClick={onDelete}>
-            Delete
-          </Button>
 
         </Card.Footer>
-      </Stack>
+      </Box>
+      <Box width="32" p="2">
+        <VStack>
+          <Box>
+            <IconButton
+              onClick={onUpload}
+              title="Upload/Create Asset"
+              variant="ghost"
+              rounded="full"
+              color={{base: 'colorPalette.500', _hover: 'colorPalette.500'}}
+              size="sm"
+            >
+              <MdUpload></MdUpload>
+            </IconButton>
+            <IconButton
+              onClick={onReveal}
+              title="Reveal in explorer"
+              variant="ghost"
+              rounded="full"
+              color={{base: 'colorPalette.500', _hover: 'colorPalette.500'}}
+              size="sm"
+            >
+              <MdFolder></MdFolder>
+            </IconButton>
+            <IconButton
+              onClick={onDelete}
+              variant="ghost"
+              rounded="full"
+              color={{base: 'colorPalette.500', _hover: 'colorPalette.500'}}
+              size="sm"
+            >
+              <MdDelete></MdDelete>
+            </IconButton>
+          </Box>
+          <Box self-align="end">
+            <Field>
+              <Switch size="xs" colorPalette="green" checked={isAutoSave} onChange={onChangeIsAutoSave}>
+                save
+              </Switch>
+            </Field>
+          </Box>
+        </VStack>
+      </Box>
     </Card.Root>
   )
 }
